fd23f84cabc658cb8a6d468eddd023d5506a3ae5
Acasa <eduard-adrian.polodeanu@student.tuiasi.ro>
2023-03-29 23:51:54 +0300

    lab5-completare

--- js/tableInsert.js
+++ js/tableInsert.js
@@ -3,6 +3,19 @@ function addRow() {
     var color = document.getElementById("culoareCelula").value;
     var insertPosition = document.getElementById("pozitieCelula").value;
     
+    var row = table.insertRow(insertPosition);
+    if (Number.isInteger(parseInt(insertPosition))) {
+        for (var i = 0; i < table.rows[0].cells.length; i++) {
+        var cell = row.insertCell(i);
+        cell.innerHTML = "Celula noua";
+        cell.bgColor = color;
+        }
+    }
+    else {
+        document.getElementById("mesaj-valoare-incorecta").innerHTML =
+         "Valoarea adăugată nu este validă. Introduceți un număr întreg. Refresh to update.";
+    }
+    
 }
 
 function addColumn() {
@@ -10,4 +23,16 @@ function addColumn() {
     var color = document.getElementById("culoareCelula").value;
     var insertPosition = document.getElementById("pozitieCelula").value;
 
+    if (Number.isInteger(parseInt(insertPosition))) {
+        for (var i = 0; i < table.rows.length; i++) {
+            var cell = table.rows[i].insertCell(insertPosition);
+            cell.innerHTML = "Celula noua";
+            cell.bgColor = color;
+        }
+    }
+    else {
+        document.getElementById("mesaj-valoare-incorecta").innerHTML =
+         "Valoarea adăugată nu este validă. Introduceți un număr întreg. Refresh to update.";
+    }
+        
 }
